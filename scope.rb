b08a761f599cd098976934f99d08c4ecef9d74ac
Michael Dorrance <dorrancemd@gmail.com>
2015-05-29 13:34:26 -0600

    methods look for loval variable first then methods that equal the variable name

--- scope.rb
+++ scope.rb
@@ -1,34 +1,55 @@
-# x is the variable outside the scope of the method but has access
-# because it is passed in the method params
-x = 10
-puts "x is #{x}"
+# # x is the variable outside the scope of the method but has access
+# # because it is passed in the method params
+# x = 10
+# puts "x is #{x}"
+#
+# x += 20
+#
+# puts "x is #{x}"
+#
+# def  print_doubled_value(x)
+#   orig = x
+#   x = x * 2
+#   puts "double the value of #{orig} is #{x}"
+#   puts "inner x is now #{x}"
+# end
+#
+# print_doubled_value(x)
+#
+# y = 27
+#
+# print "outer x is: #{x}"
+#
+##**************************************************
+#
+# # b is NOT passed in because it is not in the combine_var(a)
+# # b is locked out by the method call
+# a = 4
+# b = 12
+#
+# def dombine_var(x)
+#   puts "inner x is #{x}"
+#   puts "and outer b is #{b}"
+# end
+#
+# combine_var(a)
 
-x += 20
+#******************************************************
 
-puts "x is #{x}"
-
-def  print_doubled_value(x)
-  orig = x
-  x = x * 2
-  puts "double the value of #{orig} is #{x}"
-  puts "inner x is now #{x}"
+# methods can call in other methods as variables if NO local variable is present
+def combine_variables(x)
+  puts "inner x is: #{x}"
+  puts "and outer b is: #{b}"
 end
 
-print_doubled_value(x)
+def b
+  12
+end
 
-y = 27
+b = 2
 
-print "outer x is: #{x}"
-
-
-# b is NOT passed in because it is not in the combine_var(a)
-# b is locked out by the method call
 a = 4
-b = 12
 
-def dombine_var(x)
-  puts "inner x is #{x}"
-  puts "and outer b is #{b}"
-end
+combine_variables(a)
 
-combine_var(a)
+#*******************************************************
