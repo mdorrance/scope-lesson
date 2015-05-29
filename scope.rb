544484327ea748aee4bb502e225b6b9f6ddc71c7
Michael Dorrance <dorrancemd@gmail.com>
2015-05-29 13:25:57 -0600

    shows how variable scope locks out similar named variables

--- scope.rb
+++ scope.rb
@@ -1,3 +1,5 @@
+# x is the variable outside the scope of the method but has access
+# because it is passed in the method params
 x = 10
 puts "x is #{x}"
 
@@ -17,3 +19,16 @@ print_doubled_value(x)
 y = 27
 
 print "outer x is: #{x}"
+
+
+# b is NOT passed in because it is not in the combine_var(a)
+# b is locked out by the method call
+a = 4
+b = 12
+
+def dombine_var(x)
+  puts "inner x is #{x}"
+  puts "and outer b is #{b}"
+end
+
+combine_var(a)
